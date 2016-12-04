99fbe46d3f9bacec0d8c6318925fd59b473d2402
Kendall Weistroffer <kendall.fonebone@gmail.com>
2016-12-03 17:42:28 -0700

    hopefully this works, changing padding/text loc

--- index.php
+++ index.php
@@ -37,8 +37,8 @@ if (isset($_SESSION["userid"])) {
 				
 				<div class="row omb_row-sm-offset-3">
 					<div class="col-xs-12 col-sm-3">
-						<label class="checkbox">
-							<input type="checkbox" value="remember-me" style= "padding-left: 100px; text-color: black">Remember Me
+						<label class="checkbox" style= "padding-left: 30px; text-color: black">
+							<input type="checkbox" value="remember-me">Remember Me
 						</label>
 					</div>
 					<div class="col-xs-12 col-sm-3">
