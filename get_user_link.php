06c6a4e9f5dfe070a2dd6211ff39c1bb37a10ad9
David Chan <DavidMChan@users.noreply.github.com>
2016-12-03 20:17:48 -0700

    Update get_user_link.php

--- get_user_link.php
+++ get_user_link.php
@@ -3,14 +3,11 @@
     $userid = $_SESSION['userid']; 
     $con = mysqli_connect("localhost", "platypus", "password") or die(mysqli_error($con));
     mysqli_select_db($con, "platipi") or die(mysqli_error($con));
-
     $qq = mysqli_query($con, "SELECT users.group_link FROM users WHERE users.userid=".$userid);
-
     if (!$qq) {
         $message  = 'Invalid query: ' . mysqli_error($con) . "\n";
         die($message);
     }
     $qq_row = mysqli_fetch_array($qq);
-
     echo $qq_row[0]
 ?>
